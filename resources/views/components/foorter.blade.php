e76359d6173425496b1b165566aeedae92169c23
Dimas Trivianto <60378508+dimastrivianto@users.noreply.github.com>
2023-03-16 12:09:24 +0000

    seperate pages

---
+++ resources/views/components/foorter.blade.php
@@ -0,0 +1,24 @@
+<footer>
+     	<div class="row">
+
+     		<div class="col-six tab-full pull-right social">
+
+     			<ul class="footer-social">
+			      <li><a href="https://www.facebook.com/dimas.trivianto"><i class="fa fa-facebook"></i></a></li>
+			      <li><a href="https://github.com/dimastrivianto"><i class="fa fa-github"></i></a></li>
+			      <li><a href="https://twitter.com/dimastrivianto"><i class="fa fa-twitter"></i></a></li>
+			      <li><a href="https://www.linkedin.com/in/dimas-trivianto-b215751ab/"><i class="fa fa-linkedin"></i></a></li>
+			      <li><a href="https://www.instagram.com/dimastrivianto90/"><i class="fa fa-instagram"></i></a></li>
+			   </ul>
+
+	      </div>
+
+      	<div class="col-eight tab-full">
+	      	
+
+	      	<div id="go-top">
+		         <a class="smoothscroll" title="Back to Top" href="#top"><i class="fa fa-long-arrow-up"></i></a>
+		      </div>
+
+      	</div> <!-- /row -->
+   </footer>
