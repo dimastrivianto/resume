eea4136f3398f7ba6a1e284272a026640605428b
Dimas Trivianto <60378508+dimastrivianto@users.noreply.github.com>
2023-03-16 10:37:22 +0000

    medsos link

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -991,11 +991,11 @@
      		<div class="col-six tab-full pull-right social">
 
      			<ul class="footer-social">
-			      <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-			      <li><a href="#"><i class="fa fa-behance"></i></a></li>
-			      <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-			      <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
-			      <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+			      <li><a href="https://www.facebook.com/dimas.trivianto"><i class="fa fa-facebook"></i></a></li>
+			      <li><a href="https://github.com/dimastrivianto"><i class="fa fa-square-github"></i></a></li>
+			      <li><a href="https://twitter.com/dimastrivianto"><i class="fa fa-twitter"></i></a></li>
+			      <li><a href="https://www.linkedin.com/in/dimas-trivianto-b215751ab/"><i class="fa fa-linkedin"></i></a></li>
+			      <li><a href="https://www.instagram.com/dimastrivianto90/"><i class="fa fa-instagram"></i></a></li>
 			   </ul>
 
 	      </div>
