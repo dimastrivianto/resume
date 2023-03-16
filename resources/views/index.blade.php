780d5fefb10aaffe4aa1eb135f57a6c8bb1791dc
Dimas Trivianto <60378508+dimastrivianto@users.noreply.github.com>
2023-03-16 10:39:06 +0000

    medsos link sticky

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -88,11 +88,11 @@
    	</div> <!-- /intro-content -->
 
    	<ul class="intro-social">
-         <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-         <li><a href="#"><i class="fa fa-behance"></i></a></li>
-         <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-         <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
-         <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+	   <li><a href="https://www.facebook.com/dimas.trivianto"><i class="fa fa-facebook"></i></a></li>
+			      <li><a href="https://github.com/dimastrivianto"><i class="fa fa-square-github"></i></a></li>
+			      <li><a href="https://twitter.com/dimastrivianto"><i class="fa fa-twitter"></i></a></li>
+			      <li><a href="https://www.linkedin.com/in/dimas-trivianto-b215751ab/"><i class="fa fa-linkedin"></i></a></li>
+			      <li><a href="https://www.instagram.com/dimastrivianto90/"><i class="fa fa-instagram"></i></a></li>
       </ul> <!-- /intro-social -->
 
    </section> <!-- /intro -->
